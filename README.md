6f6df1f9d37ab77bb32e711ccd7ff0e7a169fa76
bezel452 <1647791337@qq.com>
2024-12-15 17:10:32 +0800

    Add the figure drawing script

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # VFL with SplitNN
-CS in Xi‘an Jiao Yong University
+CS in Xi‘an Jiao Tong University
 
-It is the repository of code of the simulation of multi-participant's VFL with Split Neuron Network
+## Description
+
+It is the repository of code of the simulation of multi-participant's VFL with Split Neuron Network.
+
+
+
+## Others
+
+はいはい、これはセイアンコーツー大学　コンピューターサイエンス大学院　の　大学院生　bezelです。
+
+ここは「Vertical Federated Learning のシミュレーション」と、コードストレージするリポジトリです。
+
+それにしても、どうな言語は難しいですねー、コンピューターのも人類のも。
+
--- recipe/drawFigure.py
+++ recipe/drawFigure.py
@@ -1,4 +1,37 @@
 import matplotlib.pyplot as plt
 import csv
+import time
+import argparse
 
+class drawFigure:
+    def __init__(self, num_client, csv_path, epochs, dataset):
+        self.num = num_client
+        self.csv = csv_path
+        self.x = [_ for _ in range(1, epochs + 1)]
+        self.d = dataset
+    
+    def drawLossFigure(self):
+        with open(self.csv, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                V_data = row
+        V_data = list(map(float, V_data))
+        t = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime())
+        filename = self.d + '_' + str(self.num) + '_'+ str(len(self.x)) + '_' + t + '.png'
+        plt.plot(self.x, V_data)
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.title('Figure of Loss')
+        plt.savefig(filename)
+        plt.show()
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='It is the simulation of VFL with SplitNN')
+    parser.add_argument('-n', '--num_client', default=2, type=int, help='The number of clients in the Simulation')
+    parser.add_argument('-d', '--dataset', default='cifar10', type=str, help='The dataset used in the Simulation')
+    parser.add_argument('-e', '--epochs', default=20, type=int, help='The epochs of training')
+    parser.add_argument('-f', '--file_path', default=None, type=str, help='The path of csv file')
+    args = parser.parse_args()
+    draw = drawFigure(args.num_client, args.file_path, args.epochs, args.dataset)
+    draw.drawLossFigure()
+        
